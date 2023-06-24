993659c3ae1c8978539aedb54707535c0af6c975
julia doroshina <yuspiri@gmail.com>
2023-06-24 12:20:16 +0200

    Render contractors list

---
+++ js/contractors.js
@@ -0,0 +1,77 @@
+
+const contractorTemplate = document
+  .querySelector('#user-table-row__template')
+  .content.querySelector('.users-list__table-row');
+const contractorsContainer = document.querySelector('.users-list__table-body');
+
+const getPaymentMethods = (methods, element) => {
+  const paymentMethodsContainer = element.querySelector('.payment-methods-list');
+  paymentMethodsContainer.innerHTML = '';
+
+  if (Array.isArray(methods) && methods.length > 0) {
+    const listItems = methods.map((method) => `<li class="users-list__badges-item badge">${method.provider}</li>`);
+    paymentMethodsContainer.innerHTML = listItems.join('');
+  }
+};
+
+const createContractorsProfile = (contractors) => {
+  contractors.forEach((contractor) => {
+    const contractorElement = contractorTemplate.cloneNode(true);
+
+    // const tableBtn = contractorElement.querySelector('button');
+    contractorElement.querySelector('#contractor-name').textContent = contractor.userName;
+    contractorElement.querySelector('#contractor-verified-icon').style.display = contractor.isVerified ? 'block' : 'none';
+    contractorElement.querySelector('.contractor-currency').textContent = contractor.balance.currency;
+    contractorElement.querySelector('.contractor-echange-rate').textContent = `${contractor.exchangeRate} ₽`;
+    getPaymentMethods(contractor.paymentMethods, contractorElement);
+    contractorsContainer.appendChild(contractorElement);
+  });
+};
+
+
+export { createContractorsProfile };
+// [
+// 	{
+// 		"id": "0CsQ60w3M-RTyumLjjlfN",
+// 		"balance": {
+// 			"currency": "KEKS",
+// 			"amount": 4.94
+// 		},
+// 		"exchangeRate": 2517204.54,
+// 		"isVerified": false,
+// 		"status": "seller",
+// 		"userName": "Зинаида",
+// 		"paymentMethods": [
+// 			{
+// 				"currency": "RUB",
+// 				"provider": "Sberbank",
+// 				"accountNumber": "0000 0000 0000 4991"
+// 			},
+// 			{
+// 				"currency": "RUB",
+// 				"provider": "Cash in person"
+// 			}
+// 		],
+// 	"coords": {
+// 		"lat": 59.65203,
+// 		"lng": 30.24462
+// 	},
+// 	"minAmount": 7906
+// 	},
+// 	{
+// 		"id": "1ctUkL5VGbv5x126ypZ0n",
+// 		"balance": {
+// 			"currency": "RUB",
+// 			"amount": 120064,
+// 		},
+// 		"exchangeRate": 109485.34,
+// 		"isVerified": false,
+// 		"status": "buyer",
+// 		"userName": "Пётр",
+// 		"wallet": {
+// 			"currency": "KEKS",
+// 			"address": "je9dnx2c04l2tgmm1fsxphw2pikdcuksni663wn8"
+// 		},
+// 		"minAmount": 18290
+// 	}
+// ]
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getDataUser, getDataContractors } from './api.js';
 import { createUserProfile } from './user.js';
+import { createContractorsProfile } from './contractors.js';
 
 getDataUser(createUserProfile, () => {console.log('userdataalert');});//добавь вторую функцию alertmessage
-getDataContractors();
+getDataContractors(createContractorsProfile, () => {console.log('contractorsalert');});//добавь вторую функцию alertmessage
